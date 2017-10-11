30cf715d2cd3e6a9b096d3e05171c696191f525e
Will Johnson <willalanjohnson@gmail.com>
2017-10-11 15:23:36 -0600

    Added try/catch

--- bot.js
+++ bot.js
@@ -15,10 +15,26 @@ var bot = new Discord.Client({
  autorun: true
 });
 
+var randomGames = [
+  'Scattering Mercys',
+  'Going down on Pharah',
+  'Ana is a GILF',
+  'Fuck Junkrat',
+  'Widow can\'t aim',
+  'Seeing through walls',
+  'Mei is THICC',
+  'Dragon my balls on your mom\'s face',
+];
+
 bot.on('ready', function (evt) {
   logger.info('Connected');
   logger.info('Logged in as: ');
   logger.info(bot.username + ' - (' + bot.id + ')');
+
+  setInterval(() => {
+    let rand = Math.floor(Math.random() * 8);
+    bot.setPresence({ status: 'online', game: { name: randomGames[rand] }});
+  }, 60000);
 });
 
 var offeringMessages = [
@@ -74,91 +90,97 @@ var whatsBetterTriggers = [
 ];
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-  if (userID == 350043625620635648)
-    return;
+  try {
+    // don't respond to ourself
+    if (userID == bot.id)
+      return;
 
-  // checking if the user has made an offering
-  if (messageContainsTrigger(offeringTriggers, message)) {
-    var message = 'Your offering was... ';
-    // roll a die for how good the offering was
-    var result = Math.floor(Math.random() * 11);
+    // checking if the user has made an offering
+    if (messageContainsTrigger(offeringTriggers, message)) {
+      var message = 'Your offering was... ';
+      // roll a die for how good the offering was
+      var result = Math.floor(Math.random() * 11);
 
-    bot.sendMessage({
-      to: channelID,
-      message: message + offeringMessages[result]
-    });
-  }
+      bot.sendMessage({
+        to: channelID,
+        message: message + offeringMessages[result]
+      });
+    }
 
-  // checking if the user has asked for forgiveness
-  if (messageContainsTrigger(apologyTriggers, message)) {
-    var result = Math.floor(Math.random() * 5);
+    // checking if the user has asked for forgiveness
+    if (messageContainsTrigger(apologyTriggers, message)) {
+      var result = Math.floor(Math.random() * 5);
 
-    bot.sendMessage({
-      to: channelID,
-      message: apologyMessages[result]
-    });
-  }
+      bot.sendMessage({
+        to: channelID,
+        message: apologyMessages[result]
+      });
+    }
 
-  if (messageContainsTrigger(helloTriggers, message)) {
-    var result = Math.floor(Math.random() * 4);
-    bot.sendMessage({
-      to: channelID,
-      message: helloMessages[result] + '<@' + userID + '>'
-    });
-  }
+    if (messageContainsTrigger(helloTriggers, message)) {
+      var result = Math.floor(Math.random() * 4);
+      bot.sendMessage({
+        to: channelID,
+        message: helloMessages[result] + '<@' + userID + '>'
+      });
+    }
 
-  if (message.toLowerCase().indexOf('flip a coin') != -1) {
-    var result = Math.random();
-    var message = 'Tails.';
-    if (result < .5)
-      message = 'Heads.';
+    if (message.toLowerCase().indexOf('flip a coin') != -1) {
+      var result = Math.random();
+      var message = 'Tails.';
+      if (result < .5)
+        message = 'Heads.';
 
-    bot.sendMessage({
-      to: channelID,
-      message
-    });
-  }
-
-  if (message.toLowerCase().indexOf('whats better') != -1 ||
-      message.toLowerCase().indexOf('what\'s better') != -1 ||
-      message.toLowerCase().indexOf('which is better') != -1) {
-    var result = Math.random();
-    var words = message.toLowerCase().split(' ');
-    var orIndex = words.indexOf('or');
-    // bad message format. return.
-    if (orIndex == -1 || orIndex == 0 || orIndex == words.length - 1) {
-      message = 'Ask me a real question.';
       bot.sendMessage({
         to: channelID,
         message
       });
-      return;
     }
 
-    var firstOption = words[orIndex - 1];
-    var secondOption = words[orIndex + 1];
-    if (firstOption != 'dragon' && firstOption != 'dragons' && secondOption != 'dragon' && secondOption != 'dragons') {
-      // message didnt contain 'dragon' or 'dragons' as an option
-      message = (Math.random() < .50 ? capitalize(stripPunch(firstOption)) : capitalize(stripPunc(secondOption))) + '. But dragons are better than both.';
+    if (message.toLowerCase().indexOf('whats better') != -1 ||
+        message.toLowerCase().indexOf('what\'s better') != -1 ||
+        message.toLowerCase().indexOf('which is better') != -1) {
+      var result = Math.random();
+      var words = message.toLowerCase().split(' ');
+      var orIndex = words.indexOf('or');
+      // bad message format. return.
+      if (orIndex == -1 || orIndex == 0 || orIndex == words.length - 1) {
+        message = 'Ask me a real question.';
+        bot.sendMessage({
+          to: channelID,
+          message
+        });
+        return;
+      }
+
+      var firstOption = words[orIndex - 1];
+      var secondOption = words[orIndex + 1];
+      if (firstOption != 'dragon' && firstOption != 'dragons' && secondOption != 'dragon' && secondOption != 'dragons') {
+        // message didnt contain 'dragon' or 'dragons' as an option
+        message = (Math.random() < .50 ? capitalize(stripPunch(firstOption)) : capitalize(stripPunc(secondOption))) + '. But dragons are better than both.';
+        bot.sendMessage({
+          to: channelID,
+          message
+        });
+        return;
+      }
+
+      var otherOption = firstOption;
+      message = 'Dragons.'
+      if (firstOption == 'dragon' || firstOption == 'dragons')
+        otherOption = capitalize(stripPunc(secondOption));
+
+      if (result <= 0.03)
+        message = otherOption + '.';
+
       bot.sendMessage({
         to: channelID,
         message
       });
-      return;
     }
-
-    var otherOption = firstOption;
-    message = 'Dragons.'
-    if (firstOption == 'dragon' || firstOption == 'dragons')
-      otherOption = capitalize(stripPunc(secondOption));
-
-    if (result <= 0.03)
-      message = otherOption + '.';
-
-    bot.sendMessage({
-      to: channelID,
-      message
-    });
+  } 
+  catch (e) {
+    logger.error(`Error: ${e}`);
   }
 });
 
@@ -180,3 +202,8 @@ function capitalize(string) {
 function stripPunc(string) {
   return string.replace(/[.',\/#!$%?\^&\*;:{}=\-_`~()]/g, '');
 }
+
+bot.on('disconnect', function(msg, code) {
+  logger.info(`Bot disconnected from Discord with code ${code}, message: ${msg}.`)
+  bot.connect();
+});
